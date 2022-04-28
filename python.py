3e5c7a1617cb26aed9b6d03b8ba6bccb5b3dfba8
paull04 <42182054+paull04@users.noreply.github.com>
2022-04-28 21:59:14 +0900

    Initial commit

---
+++ python.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+arr = np.zeros([7, 7, 4])
+
+
+class Hamster:
+    def __init__(self, pos):
+        self.arr = np.zeros([7, 7, 4])
+        self.visit = np.zeros([7, 7])
+        self.pos = pos
+
+    def go(self):
+        pass
+
+    def left(self):
+        pass
+
+    def right(self):
+        pass
+
+    
